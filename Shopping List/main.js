3a4b87cb2f147bf9223f653f2e5ec903aee11bf1
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-19 21:51:06 +0100

    Add files via upload

---
+++ Shopping List/main.js
@@ -0,0 +1,9 @@
+const myList = [
+    ["Oranges", 5],
+    ["Coffee", 2],
+    ["Milk", 1],
+    ["Chips", 1],
+    ["Tea", 1]
+];
+
+console.log(myList);
---
+++ Word Blanks/main.js
@@ -0,0 +1,11 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "Everytime, my " + myNoun + " in the kitchen would "
+    + myAdverb + " " + myVerb + " away if there is my wonderfull " + myAdjective + " cat";
+console.log(wordBlanks);
+// Change this line
+// Only change code above this line
